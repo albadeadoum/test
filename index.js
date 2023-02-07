d1dcfd02498b1419e37ee62976056f3d6950ced9
albadeadoum <albadeadoum9@gmail.com>
2023-02-07 20:43:28 +0100

    premier

---
+++ index.js
@@ -0,0 +1,18 @@
+class agent {
+
+    constructor(matricule, nom, prenom, dateNaissance,email) {
+      this.matricule = matricule
+      this.nom = nom;
+      this.prenom = prenom;
+      this.dateNaissance = dateNaissance;
+      this.email = email;
+    }
+  }
+
+  class Enquete {
+
+    constructor(numero, nom) {
+      this.numero = numero
+      this.nom = nom;
+    }
+  }
